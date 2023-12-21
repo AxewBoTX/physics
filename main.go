7d1339e876a4f8efaa3f4f424406ed255689f32e
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-12-21 18:20:55 +0530

    Implementing physical World

--- main.go
+++ main.go
@@ -15,15 +15,21 @@ func main() {
 	const FPS = 60
 	raylib.SetTargetFPS(FPS)
 
-	body := src.NewBody(WIDTH/2, HEIGHT/2, 15)
+	world := src.NewWorld()
+	world.AddBody(src.NewBody(WIDTH/2, HEIGHT/2, 15))
 
 	for !raylib.WindowShouldClose() && !raylib.IsKeyPressed(raylib.KeyQ) {
 		raylib.BeginDrawing()
 		raylib.ClearBackground(raylib.RayWhite)
 		raylib.DrawFPS(10, 10)
 
-		body.Draw()
-		body.Update()
+		for i := range world.Bodies {
+			world.Bodies[i].Draw()
+			src.DebugBody(&world.Bodies[i])
+			world.Bodies[i].Update()
+			world.Bodies[i].VelDirection()
+			world.GenerateFirction()
+		}
 
 		raylib.EndDrawing()
 	}
