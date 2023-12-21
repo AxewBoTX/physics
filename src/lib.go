e858fb18bc5e51a9b332ad003f3d34ca98557347
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-12-21 18:22:21 +0530

    Function for DrawRotationalLine && DebugBody

--- src/lib.go
+++ src/lib.go
@@ -3,10 +3,34 @@ package src
 // Important functions and extensions of raylib-go
 
 import (
+	"fmt"
 	"math"
 
 	raylib "github.com/gen2brain/raylib-go/raylib"
 )
+
+// Debug Body
+func DebugBody(body *Body) {
+	raylib.DrawText(
+		fmt.Sprintf(`Vel_X: %f | Vel_Y: %f`, body.Vel.X, body.Vel.Y), 10, 30, 25, raylib.DarkGreen,
+	)
+}
+
+// Drawing Rotatable Line
+func DrawRotationalLine(location *Vector, length, angle float64) {
+	var Points [2]Vector
+	Points[0] = *NewVector(location.X+length*math.Cos(angle), location.Y-length*math.Sin(angle))
+	Points[1] = *NewVector(location.X-length*math.Cos(angle), location.Y+length*math.Sin(angle))
+	raylib.DrawLine(
+		int32(
+			Points[0].X,
+		),
+		int32(Points[0].Y),
+		int32(Points[1].X),
+		int32(Points[1].Y),
+		raylib.Black,
+	)
+}
 
 // Drawing Rotatable Rectangle
 func DrawRotationalRect(location *Vector, width, height, angle float64) {
@@ -56,12 +80,6 @@ func DrawRotationalRect(location *Vector, width, height, angle float64) {
 		int32(Points[3].Y),
 		int32(Points[0].X),
 		int32(Points[0].Y),
-		raylib.Black,
-	)
-	raylib.DrawCircleLines(
-		int32(location.X),
-		int32(location.Y),
-		float32(radius),
 		raylib.Black,
 	)
 }
