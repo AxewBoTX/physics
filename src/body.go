556df44dd0d2a181e0accc3eef3f8c179b2f1962
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-12-21 18:21:43 +0530

    Reducing code clutter

--- src/body.go
+++ src/body.go
@@ -1,5 +1,7 @@
 package src
 
+// Physical Body
+
 import (
 	"math"
 
@@ -7,14 +9,13 @@ import (
 )
 
 type Body struct {
-	Mass     float64
-	Width    float64
-	Height   float64
-	Location Vector
-	Vel      Vector
-	Acc      Vector
-	Points   [4]Vector
-	Rotation float64
+	Mass          float64
+	Width         float64
+	Height        float64
+	Location      Vector
+	Vel           Vector
+	Acc           Vector
+	RotationAngle float64
 }
 
 func NewBody(x, y, mass float64) *Body {
@@ -23,35 +24,35 @@ func NewBody(x, y, mass float64) *Body {
 
 // Drawing Function
 func (body *Body) Draw() {
-	DrawRotationalRect(&body.Location, body.Width, body.Height, body.Rotation)
+	DrawRotationalRect(&body.Location, body.Width, body.Height, body.RotationAngle)
 }
 
 // Updating Body State
 func (body *Body) Update() {
-	body.Vel.Limit(15)
+	body.HandleForceStacking()
+	body.Keys()
+}
+
+// Handle Multiple Forces on Body
+func (body *Body) HandleForceStacking() {
+	body.Vel.Limit(10)
 	body.Location.Add(body.Vel)
 	body.Vel.Add(body.Acc)
 	body.Acc.Multi(0)
-	body.VelDirection()
-	body.Keys()
-
-	// //Friction
-	// body.ApplyForce(
-	// 	*body.Vel.Get().Multi(-1).Multi(0.2),
-	// )
-}
-
-func (this *Body) Attract(other *Body) {
-	dir := SubVectors(this.Location, other.Location)
-	distSQ := math.Max(math.Min(dir.Mag()*dir.Mag(), 25), 2500)
-	strength := this.Mass * other.Mass * 0.3 / distSQ
-	dir.Multi(strength)
-	other.ApplyForce(*dir)
 }
 
 // Apply Force To The Body
 func (body *Body) ApplyForce(force Vector) {
 	body.Acc.Add(*Vector_Div(force, body.Mass))
+}
+
+// Attract other Body
+func (body *Body) Attract(other *Body) {
+	dir := SubVectors(body.Location, other.Location)
+	distSQ := math.Max(math.Min(dir.Mag()*dir.Mag(), 25), 2500)
+	strength := body.Mass * other.Mass * 0.3 / distSQ
+	dir.Multi(strength)
+	other.ApplyForce(*dir)
 }
 
 // Display Velocity Direction
@@ -68,22 +69,22 @@ func (body *Body) VelDirection() {
 	)
 }
 
-// Keyboard Input
+// Handling Keyboard Keyboard Input
 func (body *Body) Keys() {
 	if raylib.IsKeyDown(raylib.KeySpace) {
-		body.Rotation += (math.Pi / 180) * 2
+		body.RotationAngle += (math.Pi / 180) * 2
 	}
 	if raylib.IsKeyDown(raylib.KeyBackspace) {
-		body.Rotation -= (math.Pi / 180) * 2
+		body.RotationAngle -= (math.Pi / 180) * 2
 	}
 	if raylib.IsKeyDown(raylib.KeyD) {
-		body.Location.X += 5
+		body.ApplyForce(*NewVector(5, 0))
 	} else if raylib.IsKeyDown(raylib.KeyA) {
-		body.Location.X -= 5
+		body.ApplyForce(*NewVector(-5, 0))
 	}
 	if raylib.IsKeyDown(raylib.KeyS) {
-		body.Location.Y += 5
+		body.ApplyForce(*NewVector(0, 5))
 	} else if raylib.IsKeyDown(raylib.KeyW) {
-		body.Location.Y -= 5
+		body.ApplyForce(*NewVector(0, -5))
 	}
 }
